96fb7ea2f22742f2f82377f7ec0f6bf6918ef92f
Peter Barcsak <peter.barcsak@gmail.com>
2018-02-11 15:02:15 +0000

    Comannd interpreter is private now, test.txt createdd for testing Bitmap editor, test updated

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -1,3 +1,6 @@
+require_relative './bitmap.rb'
+require_relative './output.rb'
+
 class BitmapEditor
 
   attr_reader :bitmap
@@ -7,29 +10,32 @@ class BitmapEditor
 
     File.open(file).each do |line|
       line = line.chomp
+      command_interpreter(line)
     end
   end
 
-  def command_interpreter(line)
-    case line.slice!(0)
-    when 'I'
-      size = line.split(' ')
-      @bitmap = Bitmap.new(size)
-    when 'L'
-      args = line.split(' ')
-      @bitmap.change_pixel_colour(args)
-    when 'C'
-      @bitmap.clear_pixels
-    when 'V'
-      args = line.split(' ')
-      @bitmap.change_pixel_colours_vertically(args)
-    when 'H'
-      args = line.split(' ')
-      @bitmap.change_pixel_colours_horizontally(args)
-    when 'S'
-        puts "There is no image"
-    else
-        puts 'unrecognised command :('
+private
+
+    def command_interpreter(line)
+      case line.slice!(0)
+      when 'I'
+        size = line.split(' ')
+        @bitmap = Bitmap.new(size)
+      when 'L'
+        args = line.split(' ')
+        @bitmap.change_pixel_colour(args)
+      when 'C'
+        @bitmap.clear_pixels
+      when 'V'
+        args = line.split(' ')
+        @bitmap.change_pixel_colours_vertically(args)
+      when 'H'
+        args = line.split(' ')
+        @bitmap.change_pixel_colours_horizontally(args)
+      when 'S'
+          Output.new.show(@bitmap.pixels)
+      else
+          puts 'unrecognised command :('
+      end
     end
-  end
 end
--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -1,45 +1,10 @@
 require 'bitmap_editor'
 
 describe BitmapEditor do
-  before(:each) do
-    line = 'I 5 6'
-    @bitmap_editor = BitmapEditor.new
-    @bitmap_editor.command_interpreter(line)
-  end
-
-  describe '#command_interpreter' do
-    it 'delegates to create a new bitmap' do
-      pixels = [['O', 'O', 'O', 'O', 'O'],
-                ['O', 'O', 'O', 'O', 'O'],
-                ['O', 'O', 'O', 'O', 'O'],
-                ['O', 'O', 'O', 'O', 'O'],
-                ['O', 'O', 'O', 'O', 'O'],
-                ['O', 'O', 'O', 'O', 'O']]
-      expect(@bitmap_editor.bitmap.pixels).to eq pixels
-    end
-
-    it 'delegates to change pixel colour' do
-      line = 'L 1 3 A'
-      expect(@bitmap_editor.bitmap).to receive(:change_pixel_colour).with(['1', '3', 'A'])
-      @bitmap_editor.command_interpreter(line)
-    end
-
-    it 'delegates to clear pixels' do
-      line = 'C'
-      expect(@bitmap_editor.bitmap).to receive(:clear_pixels)
-      @bitmap_editor.command_interpreter(line)
-    end
-
-    it 'delegates to change pixels vertically' do
-      line = 'V 2 3 6 W'
-      expect(@bitmap_editor.bitmap).to receive(:change_pixel_colours_vertically).with(['2', '3', '6', 'W'])
-      @bitmap_editor.command_interpreter(line)
-    end
-
-    it 'delegates to change pixels horizontally' do
-      line = 'H 3 5 2 Z'
-      expect(@bitmap_editor.bitmap).to receive(:change_pixel_colours_horizontally).with(['3', '5', '2', 'Z'])
-      @bitmap_editor.command_interpreter(line)
+  describe '#run' do
+    it 'edits a bitmap regarding a file input' do
+      output = "OOOOO\nOOZZZ\nAWOOO\nOWOOO\nOWOOO\nOWOOO\n"
+      expect { subject.run('./examples/test.txt') }.to output(output).to_stdout
     end
   end
 end
