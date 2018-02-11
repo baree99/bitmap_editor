b1f887e3e9cf2bf226a1f9d2c2201e7c89e255bd
Peter Barcsak <peter.barcsak@gmail.com>
2018-02-11 11:53:43 +0000

    Bitmap can change the colour in a given pixel

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -6,6 +6,10 @@ class Bitmap
     @bitmap = create_bitmap(x, y)
   end
 
+  def change_colour(x, y, colour)
+    @bitmap[y-1][x-1] = colour
+  end
+
 private
     def create_bitmap(x, y)
       bitmap = []
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -7,9 +7,18 @@ describe Bitmap do
 
   describe '#initialize' do
     it 'creates a white bitmap with the given size when initialized' do
-      bitmap = [['O', 'O', 'O'],
+      expected_bitmap = [['O', 'O', 'O'],
                 ['O', 'O', 'O']]
-      expect(@bitmap.bitmap).to eq bitmap
+      expect(@bitmap.bitmap).to eq expected_bitmap
+    end
+  end
+
+  describe '#change_colour' do
+    it 'changes the colour in a give position' do
+      @bitmap.change_colour(3, 2, 'C')
+      expected_bitmap = [['O', 'O', 'O'],
+                         ['O', 'O', 'C']]
+      expect(@bitmap.bitmap).to eq expected_bitmap
     end
   end
 end
